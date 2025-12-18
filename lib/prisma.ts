264fa3f9fa307ab16defbab2589decfada758694
Bruno Duarte <duartebruno2208@gmail.com>
2025-12-18 11:07:36 -0300

    Update Prisma configuration to use DIRECT_URL and streamline prisma.ts file
    - Changed the database connection URL in prisma.config.ts to use DIRECT_URL.
    - Simplified prisma.ts by directly using DATABASE_URL in the adapter initialization.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const prisma = new PrismaClient({ adapter });
